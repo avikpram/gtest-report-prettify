ce9de765965d9111aebbc6df3f68225f78625ebc
Avik Pramanik <avikpram@users.noreply.github.com>
2019-06-18 11:05:16 +0200

    Merging of multiple xml files to one + Adding color/badges in the summary
    - Merging of multiple XML files to one if the argument is folder-path
    - Adding colour/badges in the summary to show the number of tests, passed tests and failed tests for each suite

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Creates a HTML page from a Google Test JSON or XML report.
 
 To run:
 ```python
-python -m gtest_parser PATH_TO_GOOGLE_TEST_JSON_OR_XML_FILE.json/xml
+python -m gtest_parser FILE_PATH_TO_GOOGLE_TEST_JSON_OR_XML_FILE.json/xml
+python -m gtest_parser DIR_PATH_TO_GOOGLE_TEST_JSON_OR_XML_FILES
 ```
 ![](gtest_prettify_example.gif)
--- gtest_parser.py
+++ gtest_parser.py
@@ -1,7 +1,8 @@
 import json
-import sys
+import sys, os
 from jinja2 import FileSystemLoader, Environment
 import xml.etree.ElementTree as ET
+from junitparser import *
 
 # Constants
 TEMPLATE_FILE = "gtest_template.html"
@@ -35,9 +36,11 @@ def process_xml(xml):
     tree = ET.parse(xml)
     root = tree.getroot()
     overviewName = root.attrib['name']
+    if overviewName.startswith("GTestClass_"):
+      overviewName = overviewName[11:]   
     overviewTests = int(root.attrib['tests'])
     overviewFailed = int(root.attrib['failures'])
-    overviewDisabled = int(root.attrib['disabled'])
+    overviewDisabled = 0 # int(root.attrib['disabled'])
     data = {
         'name': overviewName,
         'tests': overviewTests,
@@ -48,6 +51,8 @@ def process_xml(xml):
 
     for child in root:
         testSuitename = child.attrib['name']
+        if testSuitename.startswith("GTestClass_"):
+          testSuitename = testSuitename[11:]         
         totalTests = int(child.attrib['tests'])
         failed = int(child.attrib['failures'])
         disabled = int(child.attrib['disabled'])
@@ -95,13 +100,58 @@ def create_html(data):
     """ Turns the JSON object into a HTML file.
         Will grab the template and render it with our JSON object.
     """
-    templateLoader = FileSystemLoader(searchpath="./")
+    templateLoader = FileSystemLoader(searchpath="./template/")
     templateEnv = Environment(loader=templateLoader)
     template = templateEnv.get_template(TEMPLATE_FILE)
+    
+    if not os.path.exists(os.path.join('.', 'out')):
+      os.makedirs(os.path.join('.', 'out'))    
 
-    with open(OUTPUT_FILE, "w") as output_html:
+    with open(os.path.join('.', 'out', OUTPUT_FILE), "w") as output_html:
         output_html.write(template.render(test_overview=data, test_suites=data['testsuites']))
 
 if __name__ == "__main__":
-    json_data = process_input(sys.argv[1])
+    if os.path.isdir(sys.argv[1]):  
+        input_path = sys.argv[1]
+        print "\nCombine all available xml files into one\n"
+        
+        try:
+            os.remove(os.path.join(input_path, 'temp', 'merged.xml'))
+        except OSError:
+            pass
+    
+        xml_list = []
+        mergedXml = JUnitXml()
+        test_num = 0
+        fail_num = 0
+        disable_num = 0
+      
+        for f in os.listdir(input_path):
+          if f.endswith(".xml"):
+            xml_list.append(os.path.join(input_path, f))
+            
+        for f in xml_list:
+          newXml = JUnitXml.fromfile(f)
+          test_num += newXml.tests
+          fail_num += newXml.failures
+          # disable_num += newXml.skipped
+          # print newXml.skipped
+          
+          mergedXml = mergedXml + JUnitXml.fromfile(f)
+            
+        mergedXml.name = 'AllTests'
+        mergedXml.tests = test_num
+        mergedXml.failures = fail_num
+        # mergedXml.skipped = disable_num
+        # mergedXml.disabled = IntAttr('disabled')
+        if not os.path.exists(os.path.join(input_path, 'temp')):
+          os.makedirs(os.path.join(input_path, 'temp'))
+    
+        mergedXml.write(os.path.join(input_path, 'temp', 'merged.xml'))
+        json_data = process_input(os.path.join(input_path, 'temp', 'merged.xml'))
+        
+    else:
+        print "\nProcess single xml file\n"
+        json_data = process_input(sys.argv[1])
+        
     create_html(json_data)
